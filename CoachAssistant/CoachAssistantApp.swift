2168165ac2c79326b206ef0461e1061f2d0b1969
Brandon Koehring <curropt@yahoo.com>
2025-09-12 15:09:52 -0400

    Initial Commit

---
+++ CoachAssistant/CoachAssistantApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoachAssistantApp.swift
+//  CoachAssistant
+//
+//  Created by Brandon Koehring on 9/12/25.
+//
+
+import SwiftUI
+
+@main
+struct CoachAssistantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CoachAssistantTests/CoachAssistantTests.swift
@@ -0,0 +1,16 @@
+//
+//  CoachAssistantTests.swift
+//  CoachAssistantTests
+//
+//  Created by Brandon Koehring on 9/12/25.
+//
+
+import Testing
+
+struct CoachAssistantTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
